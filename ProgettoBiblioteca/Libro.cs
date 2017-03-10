3a2ca227519d2ce44490c94092ccb3b15b1f8784
Nyne Sherry <Nyne Sherry>
2017-03-10 06:57:18 +0100

    ciao

---
+++ ProgettoBiblioteca/Libro.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgettoBiblioteca
+{
+    class Libro
+    {
+        public string titolo { get; set; }
+        public string autore { get; set; }
+        Genere genere { get; set; }
+        public int ISBN { get; set; } 
+        public bool inPrestito { get { return inPrestito; } }
+        
+        public Libro (string titolo, string autore/*, genere*/, int  ISBN)
+        { 
+            //costruttore
+        }
+
+        public void presta (Utente u)
+        {
+            //funzione presta
+        }
+
+    }
+}
---
+++ ProgettoBiblioteca/Utente.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgettoBiblioteca
+{
+    class Utente
+    {
+        public string nome { get; set; } 
+        public string cognome { get; set; }
+        public string codiceFiscale { get; set; }
+        public DateTime nascita { get; set; }
+
+        public Utente (string nome, string cognome, string codiceFiscale, DateTime nascita)
+        {
+            //costruttore
+        }
+
+        public override string ToString()
+        {
+            return nome + " " + cognome + " " + nascita;
+        }
+
+        public string describe (/*nome, cognome, codice fiscale*/)
+        {
+            return "tutto";
+        }
+
+        //lista libriPosseduti
+
+         /*public string describeLibri ()
+        {
+            describe di ogni libro
+
+            return tutto;
+        }*/
+    }
+}
